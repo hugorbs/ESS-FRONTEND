33f9018a825f04a8f31ab9ca6f188ad2aff3ca0c
hugorbs <hrbs2@cin.ufpe.br>
2016-11-23 09:07:46 -0300

    chart

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -1,21 +1,28 @@
 angular.module('ess-app')
 .controller('UserCtrl',['$rootScope', '$scope', '$routeParams', '$document', '$location', 'AuthService', 'UserService', function($rootScope, $scope, $routeParams, $document, $location, AuthService, UserService) {
-  $scope.G1_labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.G1_series = ['Series A', 'Series B'];
+  $scope.G1_labels = ['GDI', 'ESS', 'PCOM', 'Sinais', 'Métodos'];
+  $scope.G1_series = ['Faltas', 'Presenças'];
 
   $scope.G1_data = [
-   [65, 59, 80, 81, 56, 55, 40],
-   [28, 48, 40, 19, 86, 27, 90]
+   [1, 3, 2, 6, 8],
+   [18, 15, 12, 10, 6]
   ];
 
-  $scope.G2_labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.G2_data = [300, 500, 100];
+  $scope.G2_labels = ['Faltas', 'Presenças'];
+  $scope.G2_data = [15, 40];
 
-  $scope.G3_labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.G5_labels = ['Faltas', 'Presenças'];
+  $scope.G5_data = [12, 40];
+  $scope.G6_data = [25, 32];
+  $scope.G7_data = [6, 45];
+  $scope.G8_data = [20, 35];
+  $scope.G9_data = [40, 10];
+
+  $scope.G3_labels = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho"];
   $scope.G3_series = ['Series A', 'Series B'];
   $scope.G3_data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+    [10, 12, 6, 3, 18, 1, 7],
+    [28, 48, 40, 19, 40, 30, 40]
   ];
   $scope.G3_onClick = function (points, evt) {
     console.log(points, evt);
